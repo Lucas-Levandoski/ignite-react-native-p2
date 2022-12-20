0ef783a5a64b393444341363ef7b962375e21b0b
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-12-20 13:28:05 -0300

    first commit with styled components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Text } from './styles';
+
+export default function Groups() {
+  return (
+    <Container>
+      <Text>Open up App.tsx to start on your app!</Text>
+    </Container>
+  );
+}
